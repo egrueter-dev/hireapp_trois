34f8a91ff32e25230ca04117cd18ef751215cd61
Erik Grueter <erikgrueter@gmail.com>
2015-05-09 09:16:37 -0400

    created dashboards resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :networks, only: [:index, :create]
   resources :hires, only: [:index, :create]
   resources :jobs, only: [:index, :create]
+  resources :dashboards, only: [:index, :create]
 
+  get 'company/home', to: 'dashboards#index'
   get 'company/jobs', to: 'jobs#index'
   get 'settings/networks', to: 'networks#index'
   get 'settings/profile', to: 'profile#index'
