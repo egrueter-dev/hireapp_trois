b08762308e0ea9351e2b78ed7e996e479fe86c5a
Erik Grueter <erikgrueter@gmail.com>
2015-05-09 09:06:28 -0400

    added jobs resources and custome routes

---
+++ app/controllers/hires_controller.rb
@@ -0,0 +1,4 @@
+class HiresController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 
   resources :settings, only: [:index]
   resources :networks, only: [:index, :create]
+  resources :hires, only: [:index, :create]
+  resources :jobs, only: [:index, :create]
 
+  get 'company/jobs', to: 'jobs#index'
   get 'settings/networks', to: 'networks#index'
   get 'settings/profile', to: 'profile#index'
 end
