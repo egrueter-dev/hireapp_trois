cfba5de99be2881755d3431dc8c0c6c801f835fa
Erik Grueter <erikgrueter@gmail.com>
2015-05-12 14:09:51 -0400

    adding user_id to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150512174908) do
+ActiveRecord::Schema.define(version: 20150512180750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150512174908) do
     t.integer  "views"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
