c29ce2d2de0d06f749dab96df0f12a3d4107900f
Erik Grueter <erikgrueter@gmail.com>
2015-05-12 14:04:28 -0400

    created jobs table

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'homes#index'
   post '/', to: 'homes#create'
-
   devise_for :users
 
   resources :settings, only: [:index]
   resources :networks, only: [:index, :create]
   resources :hires, only: [:index, :create]
   resources :jobs, only: [:index, :create]
+
+  #jobs needs to be updated here..
+  #/job/12321/promote must be available..
+
   resources :dashboards, only: [:index, :create]
   resources :homes, only: [:index, :create]
   resources :upgrades, only: [:index, :create, :new]
@@ -16,5 +19,4 @@ Rails.application.routes.draw do
   get 'company/jobs', to: 'jobs#index'
   get 'settings/networks', to: 'networks#index'
   get 'settings/profile', to: 'profile#index'
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509212516) do
+ActiveRecord::Schema.define(version: 20150512174908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,21 @@ ActiveRecord::Schema.define(version: 20150509212516) do
   create_table "betausers", force: :cascade do |t|
     t.string "email", null: false
     t.string "name",  null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.boolean  "open",            null: false
+    t.string   "title",           null: false
+    t.string   "employment_type", null: false
+    t.string   "industry",        null: false
+    t.string   "location",        null: false
+    t.string   "salary",          null: false
+    t.text     "description"
+    t.integer  "posts"
+    t.integer  "inquries"
+    t.integer  "views"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
