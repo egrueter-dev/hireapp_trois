adcf832b3fbdb4d0e79cd2473087e5fd8fc63f3d
Erik Grueter <erikgrueter@gmail.com>
2015-05-18 16:42:17 -0400

    working on a more robust search with both keyword and location options

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,10 +1,19 @@
 class Job < ActiveRecord::Base
   belongs_to :user
+  scope :title, lambda { |value| scope.where(title: value) }
+  scope :location, lambda { |value| scope.where(location: value) }
 
   def self.search(query)
-    where("title like ?", "%#{query}%")
+    supported_queries = [:title, :location]
+
+    binding.pry
+
+    query.slice(*supported_queries).inject(self) do |scope, (key, value)|
+      value.present? ? scope.send(key, value) : scope
+    end
+    # where("title like ?", "%#{query}%")
   end
 
   EMPLOYMENT_TYPE = ["Contract", "Full Time", "Contract to Hire", "Intern", "Part Time", "Temp"]
-  INDUSTRY = ["Accounting", "Add the rest on a rainy day"]
+  INDUSTRY = ["Accounting", "Add The Rest On A Rainy Day"]
 end
