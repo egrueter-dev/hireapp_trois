7c3cf7348200400eda0138e87d19121384688893
Erik Grueter <erikgrueter@gmail.com>
2015-05-20 12:16:43 -0400

    updated search to be 'fuzzy'

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,13 +4,14 @@ class Job < ActiveRecord::Base
   EMPLOYMENT_TYPE = ["Contract", "Full Time", "Contract to Hire", "Intern", "Part Time", "Temp"]
   INDUSTRY = ["Accounting", "Add The Rest On A Rainy Day"]
 
-  scope :title, -> (title) { where("title like ?", "#{title}") }
-  scope :location, -> (location) { where("location like ?", "#{location}") }
+  scope :title, -> (title) { where("title like ?", "%#{title}%") }
+  scope :location, -> (location) { where("location like ?", "%#{location}%") }
 
   def self.search(*args)
     if args[1] == ""
       @jobs = Job.title(args[0])
     else
+      binding.pry
       @jobs = Job.title(args[0])
       @jobs = @jobs.location(args[1])
     end
