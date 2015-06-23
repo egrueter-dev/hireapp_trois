5b0ec4c394296dc074f7362c86847ecec2826b5b
Erik Grueter <erikgrueter@gmail.com>
2015-06-23 12:51:50 -0400

    Enables inquiries for each job page

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,20 @@ Rails.application.routes.draw do
   resources :networks, only: [:index, :create]
 
   #This is the job display for the signed in user.
-  resources :jobs, only: [:index, :create, :new, :show, :edit]
+
+  resources :jobs, only: [:index, :create, :new, :show, :edit] do
+    resources :applicants, only: [:show]
+  end
 
   #Displays public job
   resources :careers, only: [:index, :show]
+  #
 
   #Jobs needs to be updated
   #/job/12321/promote must be available..
 
   resources :dashboards, only: [:index, :create]
   resources :hires, only: [:index, :create, :show]
-
   resources :homes, only: [:index, :create]
   resources :upgrades, only: [:index, :create, :new]
 
